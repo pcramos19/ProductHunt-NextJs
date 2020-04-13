c6393b7c35bc3ad2af99b72c6e9f708e018e8f26
Pedro Castañeda <pcramos19@gmail.com>
2020-04-13 11:20:44 +0200

    first commit

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAhtBhQ4JUrKkWHzUGZxMYXNKqCbwPaiug",
+    authDomain: "product-hunt-6f619.firebaseapp.com",
+    databaseURL: "https://product-hunt-6f619.firebaseio.com",
+    projectId: "product-hunt-6f619",
+    storageBucket: "product-hunt-6f619.appspot.com",
+    messagingSenderId: "904523930889",
+    appId: "1:904523930889:web:c017460b4b08f7fde887d8"
+};
+
+export default firebaseConfig;
